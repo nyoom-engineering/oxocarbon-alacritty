ec73af6255f217d7efe7434bb04fe057639f2d75
saccarosium <github.e41mv@aleeas.com>
2023-02-24 15:57:31 +0100

    Adding template with generate script

---
+++ gen.py
@@ -0,0 +1,29 @@
+import yaml
+import jinja2
+
+
+def read_theme(name: str):
+    theme = {}
+
+    with open(f"theme/base16-oxocarbon-{name}.yaml", "r") as f:
+        theme = yaml.safe_load(f)
+        f.close()
+
+    return theme
+
+
+def main():
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader("./templates/"))
+    tmp = env.get_template("scheme.yml.j2")
+    thm = read_theme('dark')
+
+    con = tmp.render(thm)
+
+    open('oxocarbon-dark.yml', 'w').close()
+
+    with open('oxocarbon-dark.yml', 'w') as f:
+        f.write(con)
+        f.close()
+
+
+main()
